0c8385436a8a8a267c82abb56f47c33a278ea673
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-03-02 22:19:35 -0300

    Criação classe filha Pessoa Fisica

--- M1S3-SistemaBanco-main/Cliente.cs
+++ M1S3-SistemaBanco-main/Cliente.cs
@@ -1,37 +1,27 @@
 
 namespace M1S3_SistemaBanco
 {
-    public class Cliente
-    {
-        public string Nome { get; set; }
-        public string CPF { get; set; }
+    public abstract class Cliente
+    { 
         public string Email { get; set; }
         public string Telefone { get; set; }
         public string Endereco { get; set; }
-        public DateTime DataNascimento { get; set; }
         public int NumeroConta { get; set; }
         public double Saldo {  get {return GetSaldo(); }  private set{} }
 
-        public int Idade { get {return (int)(Math.Floor((DateTime.Now - DataNascimento).TotalDays / 365.25)); } private set {} }
+        
         
         public List<Transacao> Extrato { get; set; }
 
-        public Cliente()
+        public Cliente(string email,string telefone, string endereco, int numeroConta)
         {
+            Email = email;
+            Telefone= telefone;
+            Endereco = endereco;
+            NumeroConta = numeroConta;
             Extrato = new List<Transacao>();
         }
-        public Cliente(string nome, string cpf, string email, string telefone, 
-                       string end, DateTime dtNascimento,int numeroConta ) :this()
-        {
-            Nome = nome;
-            CPF = cpf;
-            Email=email;
-            Telefone =telefone; 
-            Endereco = end;
-            DataNascimento = dtNascimento;
-            NumeroConta = numeroConta;
-            
-        }
+        
 
 
         public string ResumoCliente(){
---
+++ M1S3-SistemaBanco-main/PessoaFisica.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M1S3_SistemaBanco
+{
+    public class PessoaFisica : Cliente
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public int Idade { get { return (int)(Math.Floor((DateTime.Now - DataNascimento).TotalDays / 365.25)); } private set { } }
+        
+        public PessoaFisica(string nome, string cpf, string email, string telefone,
+                       string end, DateTime dtNascimento, int numeroConta) : base(email,telefone,end,numeroConta)
+    {
+        Nome = nome;
+        CPF = cpf;
+        Email = email;
+        Telefone = telefone;
+        Endereco = end;
+        DataNascimento = dtNascimento;
+        NumeroConta = numeroConta;
+
+    }
+
+    }
+    
+}
