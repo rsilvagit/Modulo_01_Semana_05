1eba42b858fa0c1d2a59afbd3ca921dd8b2c0a70
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-03-03 19:37:32 -0300

    criação Classe pessoa Fisica e ajuste Program

--- M1S3-SistemaBanco-main/PessoaFisica.cs
+++ M1S3-SistemaBanco-main/PessoaFisica.cs
@@ -12,20 +12,22 @@ namespace M1S3_SistemaBanco
         public string CPF { get; set; }
         public DateTime DataNascimento { get; set; }
         public int Idade { get { return (int)(Math.Floor((DateTime.Now - DataNascimento).TotalDays / 365.25)); } private set { } }
-        
+
+        public PessoaFisica()
+        {
+        }
         public PessoaFisica(string nome, string cpf, string email, string telefone,
                        string end, DateTime dtNascimento, int numeroConta) : base(email,telefone,end,numeroConta)
     {
         Nome = nome;
         CPF = cpf;
-        Email = email;
-        Telefone = telefone;
-        Endereco = end;
         DataNascimento = dtNascimento;
-        NumeroConta = numeroConta;
 
     }
-
+        public bool EhMaior()
+        {
+            return Idade >= 18;
+        }
     }
     
 }
